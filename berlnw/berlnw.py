ef031bc43681ed4b8cbecc15dcccea497ca39271
ebenze12 <52275898+ebenze12@users.noreply.github.com>
2020-12-04 23:12:08 +0700

    บันทึกลง arr แต่เก็บแค่ตัวสุดท้าย

--- berlnw/berlnw.py
+++ berlnw/berlnw.py
@@ -32,8 +32,19 @@ print(mycursor.rowcount, "บรรทัด ที่ลบ")
 #loop
 active_page = 26
 page = 25
+c = 1
 while active_page > page:
 
+    data_arr = {
+        c: {
+            'network':"",
+            'phone_number':"",
+            'gender':"",
+            'price':"",
+            'active':""
+        }
+    }
+    # print(data_arr)
     url = url_home + str(active_page) + "&sort_limit=1000&v="
     print("URL = ",url)
 
@@ -56,55 +67,60 @@ while active_page > page:
         ## หาข้อมูลใน TABLE
         table = soup.find("div", {"class": "tableshow"})
 
-        # # เบอร์
-        # phone_all = table.find_all("form", {"action": "/ทำนายเบอร์"})
-        # for ph in phone_all:
-        #     key_phone = ph.find('input')['value']
-        #     value_phone = ph.find("a", {"class": "phone"}).text
-        #     # print("key >>", key_phone)
-        #     print("phone >>", value_phone)
-        #
-        # # เครือข่าย
-        # img_all = table.find_all('img')
-        # for img in img_all:
-        #     img_old = str(img)
-        #     img_old_split_1 = img_old.split("_")
-        #     img_old_split_2 = img_old_split_1[-1].split(".")
-        #     network = img_old_split_2[0]
-        #     print("network >>", network)
-        #
-        # # เพศ
-        # gender_all = table.find_all("td", {"class": "gender"})
-        # for gen in gender_all:
-        #     gg = str(gen)
-        #     gg_split_1 = gg.split("<")
-        #     gg_split_2 = gg_split_1[1].split(">")
-        #     gender = gg_split_2[1]
-        #     print("GENDER >>", gender)
+        # เบอร์
+        phone_all = table.find_all("form", {"action": "/ทำนายเบอร์"})
+        for ph in phone_all:
+            key_phone = ph.find('input')['value']
+            value_phone = ph.find("a", {"class": "phone"}).text
+            # print("key >>", key_phone)
+            #print("phone >>", value_phone)
+            data_arr[c]["phone_number"] = value_phone
 
-        # # ราคา
-        # p_all = table.find_all("td", {"class": "price"})
-        # for pp in p_all:
-        #     p = str(pp)
-        #     p_split_1 = p.split(".")
-        #     p_split_2 = p_split_1[0].split(">")
-        #     price = int(p_split_2[1])
-        #     print("PRICE >>",price)
-        #
+        # เครือข่าย
+        img_all = table.find_all('img')
+        for img in img_all:
+            img_old = str(img)
+            img_old_split_1 = img_old.split("_")
+            img_old_split_2 = img_old_split_1[-1].split(".")
+            network = img_old_split_2[0]
+            #print("network >>", network)
+            data_arr[c]["network"] = network
+
+        # เพศ
+        gender_all = table.find_all("td", {"class": "gender"})
+        for gen in gender_all:
+            gg = str(gen)
+            gg_split_1 = gg.split("<")
+            gg_split_2 = gg_split_1[1].split(">")
+            gender = gg_split_2[1]
+            #print("GENDER >>", gender)
+            data_arr[c]["gender"] = gender
+
+        # ราคา
+        p_all = table.find_all("td", {"class": "price"})
+        for pp in p_all:
+            p = str(pp)
+            p_split_1 = p.split(".")
+            p_split_2 = p_split_1[0].split(">")
+            price = int(p_split_2[1])
+            #print("PRICE >>",price)
+            data_arr[c]["price"] = price
+
         # สถานะ
         status_all = table.find_all("td", {"class": "status"})
-        c = 1
         for sta in status_all:
             try:
                 key_status = sta.find('button')['id']
                 value_status = sta.find("button", {"class": "reserves"}).text
             except:
                 continue
-            print("key >>", key_status)
-            print("value",c," >>", value_status)
-            c = c+1
-        #
-        #
+            #print("key >>", key_status)
+            #print("value",c," >>", value_status)
+            data_arr[c]["active"] = value_status
+            print(data_arr)
+
+
+        c = c + 1
         active_page += 1
         page += 1
 
